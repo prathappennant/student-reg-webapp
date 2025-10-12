8806f869865dc3293d08e8b8e2b552e9e368d973
prathappennant <chaithu2211@gmail.com>
2025-10-13 01:49:00 +0530

    update cource

--- src/main/java/com/rushi/controller/HomeController.java
+++ src/main/java/com/rushi/controller/HomeController.java
@@ -91,6 +91,6 @@ public class HomeController {
     }
 
     private List<String> getCourses() {
-        return Arrays.asList("DevOps", "AWS", "Azure", "Terraform", "Kubernetes","Python","GCP",".NET");
+        return Arrays.asList("DevOps", "AWS", "Azure", "Terraform");
     }
 }

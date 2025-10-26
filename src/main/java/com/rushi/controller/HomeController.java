bce2bc7b29145a2371525f88eb11a6dc278ce912
prathappennant <chaithu2211@gmail.com>
2025-10-27 00:00:39 +0530

    corcse

--- src/main/java/com/rushi/controller/HomeController.java
+++ src/main/java/com/rushi/controller/HomeController.java
@@ -91,6 +91,6 @@ public class HomeController {
     }
 
     private List<String> getCourses() {
-        return Arrays.asList("DevOps", "AWS", "Azure", "Terraform", "Python", "Java", "GCP","CHOCKY","CHEESY","CHAITHU","MANASA");
+        return Arrays.asList("DevOps", "AWS", "Azure", "Terraform", "Python", "Java");
     }
 }
